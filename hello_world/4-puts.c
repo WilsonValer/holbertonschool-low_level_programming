1186e15a0f7dd16edf40bf95f027da780f578ad7
WilsonValer <you@example.com>
2022-06-07 13:31:13 -0700

    hello puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ * my_function - This is a description
+ */
 int main(void){
-	char str1[] = "\""Programming is like building a multilingual puzzle";
+	char str1[] = "\"Programming is like building a multilingual puzzle";
 	puts(str1);
 	return 0;
 }
