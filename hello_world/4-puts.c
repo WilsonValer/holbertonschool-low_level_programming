9767eb840e4eb8282850a27e7733ef31f75b2de0
WilsonValer <you@example.com>
2022-06-07 15:01:06 -0700

    hello

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Emtry point
- * Description: 'creating a funstion puts'
- * return:always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+	returin (0);
 }
