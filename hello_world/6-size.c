89ebe9bf99ac607ff14d521e091c57be4a2f1bb1
WilsonValer <you@example.com>
2022-06-07 17:55:28 -0700

    Sixe is not grandeur and territory

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,10 @@
 #include<stdio.h>
 int main(void)
 {
-    printf("Size of char: %zu bytes(s)\n", sizeof(char));
-    printf("Size of int: %zu bytes(s)\n", sizeof(int));
-    printf("Size of long int: %zu bytes(s)\n", sizeof(long int));
-    printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));  
-    printf("Size of float: %zu bytes(s)\n", sizeof(float));
+    printf("Size of a char: %zu bytes(s)\n", sizeof(char));
+    printf("Size of a int: %zu bytes(s)\n", sizeof(int));
+    printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));  
+    printf("Size of a float: %zu bytes(s)\n", sizeof(float));
     return (0);
 }
