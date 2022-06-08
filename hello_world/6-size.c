0401489d1ba5a676bdebe97aa37cd9123065fb10
WilsonValer <you@example.com>
2022-06-07 17:52:50 -0700

    Size is not grander

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,15 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
+
 #include<stdio.h>
 int main(void)
 {
-	int long_int;
-	int long_long_int;
     printf("Size of char: %zu bytes(s)\n", sizeof(char));
     printf("Size of int: %zu bytes(s)\n", sizeof(int));
-    printf("Size of long int: %zu bytes(s)\n", sizeof(long_int));
-    printf("Size of long long int: %zu byte(s)\n", sizeof(long_long_int));  
+    printf("Size of long int: %zu bytes(s)\n", sizeof(long int));
+    printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));  
     printf("Size of float: %zu bytes(s)\n", sizeof(float));
     return (0);
 }
