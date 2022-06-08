418d33f20168bc7f9b4ea34e7ee5bd5badfaeee7
WilsonValer <you@example.com>
2022-06-07 19:07:39 -0700

    size is not grandur and territory

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,14 +4,13 @@
  *
  * Return: Always 0 (Success)
  */
-
 #include<stdio.h>
 int main(void)
 {
-printf("Size of a char: %ld bytes(s)\n", sizeof(char));
-printf("Size of a int: %ld bytes(s)\n", sizeof(int));
-printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-printf("Size of a float: %ld bytes(s)\n", sizeof(float));
+printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+printf("Size of a int: %lu bytes(s)\n", sizeof(int));
+printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu bytes(s)\n", sizeof(float));
 return (0);
 }
