8a44ce444d1957a127b44a0671f22790ff2163e2
WilsonValer <you@example.com>
2022-06-10 07:44:27 -0700

    9. Patience, persistence and perspiration make

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+ #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+		if (ch != 57)
+		{
+			putchar(44);
+		}
+		else if (ch == 57)
+		{
+			putchar(36);
+		}
+	}
+	putchar(10);
+	return (0);
+}
