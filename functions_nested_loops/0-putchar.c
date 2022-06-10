e5cec9d460795b3bd27ec13d6513c4b04c99ff97
WilsonValer <you@example.com>
2022-06-10 13:22:36 -0700

    0. _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main - my proyect
+* betty style doc for function main goes there
+* return: always 0 (success)
+*/
+
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+return (0);
+}
