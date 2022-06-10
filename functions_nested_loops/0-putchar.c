6c0ba8288cc65baf9d286402dafc9f94df6499e1
WilsonValer <you@example.com>
2022-06-10 13:29:10 -0700

    task0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
-* main - my proyect
-* betty style doc for function main goes there
-* return: always 0 (success)
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 
 {
