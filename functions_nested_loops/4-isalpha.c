5052895ff1121fc0ad35bdec293cfb8d17446ddb
WilsonValer <you@example.com>
2022-06-10 21:49:31 -0700

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - Checks if the param is lowercase
+ *
+ * work fucntion
+ *
+ * Description: Checks if the param is lowercase.
+ *
+ * Return: 1 if is lowercase or 0 is not
+ */
+
+int _isalpha(int c)
+{
+
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+}
