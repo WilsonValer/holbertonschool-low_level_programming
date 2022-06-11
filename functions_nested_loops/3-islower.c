37a588a4c39d42b87940245352f8230931228c7d
WilsonValer <you@example.com>
2022-06-10 22:21:41 -0700

    change the comment

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * _islower(int c) - function that checks for lowercase character
- *
- *Returns 1 if c is lowercase
- *Returns 0 otherwise
+ * _islower - function that checks for lowercase character
+ *@c: the cgarcater to print
+ *Return: 1 if c is lowercase
+ *Return: 0 otherwise
  */
 
 int _islower(int c)
