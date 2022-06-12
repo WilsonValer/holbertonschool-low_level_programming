9b289d58d6da179752dc4658a987b8bd9516db78
WilsonValer <you@example.com>
2022-06-11 22:12:49 -0700

    task07

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -12,27 +12,36 @@ int print_last_digit(int n)
 {
 	int m = 0;
 	int b;
+	int result = 0;
 
 	b = abs(n);
 	if (b >= 10)
 	{
 		m = (b % 10);
+		result = m;
 
 		_putchar('0' + m);
 
-		return (m);
 	}
 	else
 	{
 		if (n == -2147483648)
 		{	m = 8;
+			result = m;
 			_putchar('0' + m);
-			return (m);
+		}
+		else if (n == 1)
+		{
+			m = 1;
+			result = 1;
+			_putchar('0' + m);
+
 		}
 		else
 		{
-			_putchar('0' + m);
-			return (b);
+			result = b;
+			_putchar('0' + b);
 		}
 	}
+	return (result);
 }
