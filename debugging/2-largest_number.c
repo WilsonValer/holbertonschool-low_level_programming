d70eb83d0377fc7c05ea646d1ec57de794398470
WilsonValer <you@example.com>
2022-06-12 18:18:54 -0700

    task02

--- debugging/2-largest_number.c
+++ debugging/2-largest_number.c
@@ -11,30 +11,23 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if ((a > b && b > c) || (a == b && c > b))
 	{
 		largest = a;
 	}
 
-	else if (a > b && c > b)
-
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
+	else if ((b > a && c > a) || (a == c && a > c))
 
 	{
 		largest = b;
 	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else if (c > b && b > a)
+
+	else if ((c > b && b > a) || (b == c && a > b))
 	{
 		largest = c;
 	}
-	else if (c > b && a > b)
+
+	else if (c == a && a == b)
 	{
 		largest = c;
 	}
