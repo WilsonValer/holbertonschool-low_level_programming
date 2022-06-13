7fcf94e21ac16ef1d7659d9b3de0efdba6c5c4de
WilsonValer <you@example.com>
2022-06-12 21:30:32 -0700

    4. isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,11 +1,14 @@
 #include "main.h"
 #include <ctype.h>
 /**
- *_isalpha  -  function that checks for alphabetic character.
- *@c: the character to print
- *return: 1 if c is a letter lowercase or uppercase
+ * _isalpha - Checks if the param is letter
+ *
+ * @c: ASCII received
+ *
+ * Description: Checks if the param is a letter.
+ *
+ * Return: If the param is a letter lowercae return 1 if not return 0
  */
-
 int _isalpha(int c)
 {
 
