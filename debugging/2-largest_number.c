6cc898cd0a03cc1d63689b90d5c5f518c3f7b1ec
WilsonValer <you@example.com>
2022-06-12 19:09:33 -0700

    2. 0 > 972?

--- debugging/2-largest_number.c
+++ debugging/2-largest_number.c
@@ -11,7 +11,8 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if ((a > b && b > c) || (a == b && c > b) || (a > b && c > b) || (a == b && b > c))
+	if ((a >= b && b > c) || (a >= b && c > b))
+
 	{
 		largest = a;
 	}
