57f3006b5a501bce16e1178703262a4767ca336f
WilsonValer <you@example.com>
2022-06-16 11:58:52 -0700

    task02

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *swap_int - print that swaps the values of two integers
+ *@a: variable received how pointer
+ *@b: variable received how pointer
+ * Return: return void
+ */
+int _strlen(char *s)
+{
+	int r;
+
+	r = strlen(s);
+	return (r);
+
+}
