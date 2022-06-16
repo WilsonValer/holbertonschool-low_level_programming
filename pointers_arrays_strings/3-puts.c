a89501a08e6577357ad739f1e30abd84a131abb2
WilsonValer <you@example.com>
2022-06-16 12:22:23 -0700

    task03

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_puts - function that prints a string, followed by a new line, to stdout
+ *@str: variable received how pointer
+ * Return: return the string r
+ */
+void _puts(char *str)
+{
+	int r;
+
+	r = puts(str);
+	return (r);
+
+}
