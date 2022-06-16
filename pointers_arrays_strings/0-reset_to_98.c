6078b8578cb4607c0aa75fd5a2984e49e8b31401
WilsonValer <you@example.com>
2022-06-16 06:33:58 -0700

    task0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * reset_to_98 - updates the value it points to to 98
- * @*n: variable received
+ * @n: variable received
  * Return: return void
  */
 void reset_to_98(int *n)
