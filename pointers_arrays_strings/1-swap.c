6ad5329f2c5db7e45eac829b4e2ec00c3a723f0e
WilsonValer <you@example.com>
2022-06-16 07:07:51 -0700

    task01

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *swap_int - print that swaps the values of two integers
+ *@a: variable received how pointer
+ *@b: variable received how pointer
+ * Return: return void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
