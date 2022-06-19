cbf5b9cb9fbdfb53b3edf15a4658b8ede7f29832
WilsonValer <you@example.com>
2022-06-19 13:10:29 -0700

    task01

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -6,19 +6,18 @@
  *@src: variable received
  * Return: return dest;
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
 	i = 0;
-	
+
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	j =0;
-
-	while ((j < n) && src[j] != '\0')
+	j = 0;
+	while ((j < n) && (src[j] != '\0'))
 	{
 		dest[i] = src[j];
 		j++;
