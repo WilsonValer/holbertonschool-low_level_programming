d33b4fcd957b87b5416f96cc68165837fd4e6869
WilsonValer <you@example.com>
2022-06-19 13:23:17 -0700

    task02

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,6 @@
  * *_strncpy - function that copies a string
  *@dest: variable received
  *@src: variable received
- @n: variable received
  * Return: return dest;
  */
 char *_strncpy(char *dest, char *src, int n)
