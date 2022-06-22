a52633381728bdfd05f2ac20390fae0ffacbbcf6
root <root@DESKTOP-JU4TAFT.localdomain>
2022-06-22 15:05:33 -0500

    argc and argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ *mul - check that multiplies two integers
+ *@a: number received
+ *@b: second number received
+ * Return: return the product about two numbers
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	
+	
+	printf("%s\n", argv[0]);
+	
+	return (0);
+}
---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ *mul - check that multiplies two integers
+ *@a: number received
+ *@b: second number received
+ * Return: return the product about two numbers
+ */
+int main (int argc)
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
