b4770438467b17ced26ced1dbf683ee0e45aa567
root <root@DESKTOP-JU4TAFT.localdomain>
2022-06-22 20:55:59 -0500

    task03

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,16 +11,17 @@ int main(int argc, char *argv[])
 {
 	int mul;
 	
-
-		if (argc > 1)
+		if (argc == 3)
 		{
 			mul = atoi(argv[1]) * atoi(argv[2]);
 			printf("%d\n",  mul);
 		}
 		else
 		{
+
 			printf("error\n");
 			return (-1);
 		}
+		
 	return (0);
 }
