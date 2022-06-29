b7c674a70c49698adf0f874f6f047b27e7d3a7f0
Wilson Valer <nederig21@gmail.com>
2022-06-29 15:18:10 -0500

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,36 +15,53 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		char *ptr;
 
 		if (s1)
-		{
-			while (s1[len_s1])
-			len_s1++;
-		}
+			len_s1 = len_string(s1);
 		if (s2)
-		{
-			while (s2[len_s2])
-			len_s2++;
-		}
-	if (n > len_s2)
+			len_s2 = len_string(s2);
+	if (n >= len_s2)
 		n = len_s2;
-
 	if (n > 0)
-		lenTotal = len_s1 + n;
+		lenTotal = len_s1 + n + 1;
 	else
 		lenTotal = len_s1 + 1;
 	if (s1 || s2)
-	ptr = malloc(sizeof(char) * lenTotal);
+		ptr = malloc(sizeof(char) * lenTotal);
 	else if (!s1 && !s2)
 		ptr = malloc(1);
-	for (i = 0; i < len_s1; i++)
+	if (!ptr)
 	{
-		ptr[i] = *s1;
-		s1++;
+		free(ptr);
+		return (NULL);
 	}
-	for (i = len_s1; i < lenTotal; i++)
+	for (i = 0; i < lenTotal; i++)
 	{
-		ptr[i] = *s2;
-		s2++;
+		if (i < len_s1)
+		{
+			ptr[i] = *s1;
+			s1++;
+		}
+		else if (i < lenTotal)
+		{
+			ptr[i] = *s2;
+			s2++;
+		}
 	}
+
 	ptr[i] = '\0';
 	return (ptr);
 }
+
+int len_string(char *string)
+{
+	int i = 0;
+
+	while (string[i])
+		i++;
+return (i);
+}
+
+
+
+
+
+
