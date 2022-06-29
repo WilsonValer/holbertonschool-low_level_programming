718b3180c9e3e0d2610a1bfbe11a09bf44d259fb
Wilson Valer <nederig21@gmail.com>
2022-06-29 17:41:40 -0500

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -18,9 +18,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (!ptr)
 		return (NULL);
-
-	for (i= 0; i < nmemb; i++)
-		ptr[i] = 0;
+	else
+	{
+		for (i = 0;i < nmemb;i++)
+			ptr[i] = 0;
+	}
 
 	return (ptr);
 
