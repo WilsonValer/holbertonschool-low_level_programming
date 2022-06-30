1437d53df53a0c74cc34f07efeba77daabd8a583
Wilson Valer <nederig21@gmail.com>
2022-06-29 19:01:46 -0500

    3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -14,9 +14,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-		
-	ptr = malloc(sizeof(int) * (max - min +1));
-	
+	ptr = malloc(sizeof(int) * (max - min + 1));
 	if (!ptr)
 		return (NULL);
 
