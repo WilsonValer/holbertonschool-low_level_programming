ff9fe0f8a82f14a198ba1c1c9fa8575b566af8c1
Wilson Valer <nederig21@gmail.com>
2022-07-01 23:56:10 -0500

    task0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
+/**
+ * struct dog - Basic atributtes of a dog
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
 struct dog
 {
 	char *name;
