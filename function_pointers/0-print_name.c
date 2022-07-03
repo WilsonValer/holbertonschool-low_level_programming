0740b7d2dcda53faf2269f4ec19374bc5a96bf9c
Wilson Valer <nederig21@gmail.com>
2022-07-03 14:06:13 -0500

    c-funtion pointer

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *print_name - check the code
+ *@name: variable
+ *@f: variable
+ *Return: Always void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+}
