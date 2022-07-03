1a362199634a498975f056668f8d7547844e3a3a
Wilson Valer <nederig21@gmail.com>
2022-07-03 15:52:24 -0500

    task01

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- *print_name - check the code
- *@name: variable
- *@f: variable
+ *array_iterator - check the code
+ *@size: variable
+ *@array: variable
+ *@action: is a funtion pointer
  *Return: Always void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	action(size);
+	size_t  i;
+
+	if (!*array)
+		return;
+	if (action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+
 }
