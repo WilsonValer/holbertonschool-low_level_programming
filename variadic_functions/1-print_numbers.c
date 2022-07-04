c49ddd3c932153bed7115b7bb7e1bc8b71234e01
Wilson Valer <nederig21@gmail.com>
2022-07-04 17:24:46 -0500

    task01

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ *print_numbers - check the code
+ *@separator: variable int
+ *@n: variable 
+ *Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	int result;
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		result = va_arg(ptr, int);
+		printf("%d", result);
+		
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ptr);
+
+}
