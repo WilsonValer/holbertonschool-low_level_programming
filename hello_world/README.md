acf92f70f8fe01d67195b9221036c49de37ecd9d
Wilson Valer <nederig21@gmail.com>
2022-07-06 21:33:27 -0500

    hacienod ppruebas

--- hello_world/README.md
+++
@@ -1,1 +0,0 @@
-we are begin programer in c 
--- recursion/main.h
+++
@@ -1,7 +0,0 @@
- #ifndef HEADER_FILE
- #define HEADER_FILE
-
-int _putchar(char c);
-void set_string(char **s, char *to);
-void _puts_recursion(char *s);
-#endif
