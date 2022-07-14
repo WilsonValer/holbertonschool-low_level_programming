ce07dd3648435e2ec5841541da1f785f9d1e727b
Wilson Valer <nederig21@gmail.com>
2022-07-14 15:59:25 -0500

    funtion that print all list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,5 +10,25 @@
  */
 size_t print_list(const list_t *h)
 {
-	return (0);
+	int i = 0;
+		const list_t *current_node = h;
+
+	if (h == NULL)
+		return (0);
+
+	while (current_node != NULL)
+	{
+		if (current_node->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%u] %s\n", current_node->len, current_node->str);
+		}
+		current_node = current_node->next;
+		i++;
+	}
+
+	return (i);
 }
