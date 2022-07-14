9cdc54510454487cd80f068a732827f926212b10
Wilson Valer <nederig21@gmail.com>
2022-07-14 16:39:00 -0500

    print the number of element of link

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *print_list - check the code
+ *@h: variable
+ * Return: Always 0.
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+		const list_t *current_node = h;
+
+	if (h == NULL)
+		return (0);
+
+	while (current_node != NULL)
+	{
+		if (current_node->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		current_node = current_node->next;
+		i++;
+	}
+
+	return (i);
+}
