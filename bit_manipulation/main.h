ce92681dbc37401d28972a88a0611842a100b9fc
Wilson Valer <nederig21@gmail.com>
2022-07-18 18:32:48 -0500

    task00

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,17 +1,10 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+
 #include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
 
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
