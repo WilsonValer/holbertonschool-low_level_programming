3b55cea1d5b3314ab2442ab6d34a682442de28f0
Wilson Valer <nederig21@gmail.com>
2022-07-18 15:16:45 -0500

    bir manipulation heads

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *binary_to_uint - check the code
+ *@b: variable
+ *Return: Always 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+
+}
+
---
+++ bit_manipulation/main.h
@@ -0,0 +1,22 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif
