a7107293cc22722813394b8fae626cc5da541b06
Wilson Valer <nederig21@gmail.com>
2022-07-18 20:22:24 -0500

    prints the binary representation of a number.

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_binary - check the code
+ *@n: variable
+ *Return: Always 0.
+ */
+void print_binary(unsigned long int n)
+{
+	long int i;
+	unsigned long int mask = sizeof(int) * 8 - 1;
+
+	for (i = mask; i >= 0; i--)
+	{
+		if (n & (1 << i))
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+
+	_putchar(10);
+}
+
