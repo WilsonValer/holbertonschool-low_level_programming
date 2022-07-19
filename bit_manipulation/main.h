263648735319b362ea3648cb30613b0f78fcf20f
Wilson Valer <nederig21@gmail.com>
2022-07-19 15:33:44 -0500

    edit bit manipulation

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <limits.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
