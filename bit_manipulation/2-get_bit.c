e27747d8bf0c0fadfabb972545aea4881875df0a
Wilson Valer <nederig21@gmail.com>
2022-07-19 18:46:49 -0500

    returns the value of a bit at a given index.

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+#include <limits.h>
+/**
+ *get_bit - check the code
+ *@n: variable
+ *Return: Always 0.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int store;
+
+	if (index > 64)
+		return (-1);
+	store = (n >> index) & 1;
+	return (store);
+}
