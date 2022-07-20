fba4d2736a8a2427d5b347d3c4a2d02541807683
Wilson Valer <nederig21@gmail.com>
2022-07-19 22:11:22 -0500

    sets the value of a bit to 0 at a given index.

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+#include <limits.h>
+/**
+ *clear_bit - check the code
+ *@index: variable
+ *@n: variable
+ *Return: Always 0.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > 64)
+		return (-1);
+
+	*n = *n & ~(1 << index);
+
+	return (1);
+}
