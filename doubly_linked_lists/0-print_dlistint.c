320d84bb0d64003bbe2c5083b45bbe540d2aed58
Wilson Valer <nederig21@gmail.com>
2022-07-23 18:09:15 -0500

    rite a function that prints all the elements of a dlistint_t list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *print_dlistint- check the code
+ *@h:variable
+ *Return: Always 0.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h && h ->prev)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		if (h->n == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%u] %s\n", h->len, h->n);
+		}
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
