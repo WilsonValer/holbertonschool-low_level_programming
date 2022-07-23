dc454e742516949fa90ba330a8ac4d40af78f314
Wilson Valer <nederig21@gmail.com>
2022-07-23 18:23:05 -0500

    task0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -10,11 +10,11 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int i = 0;
+	size_t i = 0;
 
 	if (h == NULL)
 		return (0);
-	while (h && h ->prev)
+	while (h && h->prev)
 		h = h->prev;
 
 	while (h != NULL)
