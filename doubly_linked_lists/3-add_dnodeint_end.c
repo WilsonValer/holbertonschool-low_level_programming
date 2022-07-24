667c4016b10c04150bf49c5cf821b507294d94ed
Wilson Valer <nederig21@gmail.com>
2022-07-23 23:43:11 -0500

    Write a function that adds a new node at the end of a dlistint_t list.

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,12 +12,12 @@ dlistint_t *create_nodo(const int s)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
-
 	if (new == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
+
 	new->n = s;
 	new->next = NULL;
 	new->prev = NULL;
@@ -40,7 +40,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (last_node)
 	{
 		while (last_node && last_node->next != NULL)
+		{
 			last_node = last_node->next;
+		}
+
 		last_node->next = new_node;
 		new_node->prev = last_node;
 	}
