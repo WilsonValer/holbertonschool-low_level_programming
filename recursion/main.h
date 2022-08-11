8684d3a440245bb5b11c164bc93421c99270cb88
Wilson Valer <nederig21@gmail.com>
2022-08-11 12:19:20 -0500

    header was edited

--- recursion/main.h
+++ recursion/main.h
@@ -4,7 +4,7 @@
 int _putchar(char c);
 void set_string(char **s, char *to);
 void _puts_recursion(char *s);
-oid _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s);
 int _pow_recursion(int x, int y);
 int _strlen_recursion(char *s);
 int factorial(int n);
