3085e84ba14da8b38dc3006cf975d0b5d05a7561
WilsonValer <nederig21@gmail.com>
2023-05-23 21:31:01 -0500

    was added a modification of while line

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -9,9 +9,9 @@
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	int count;
-
-	if (argc > 0)
+	if (argc > 0){
 		for (count = 0; count < argc; count++)
 			printf("%s\n", argv[count]);
-	return (0);
+		return (0);
+	}
 }
